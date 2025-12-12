57e16a59d22c638453ac4bd403e05ae893291733
johan camilo bello <johancamilobelloramirez@gmail.com>
2025-12-12 15:01:25 -0500

    final

--- introgeopandas.py
+++ introgeopandas.py
@@ -6,14 +6,5 @@ from mostrar_datos import mostrar_datos
 from exportar_datos import exportar_datos
 
 my_gdf = cargar_archivo("barrio_legalizado.json")
-mostrar_info(my_gdf)
+mostrar_mapa(my_gdf)
 exportar_datos(my_gdf,"maria cano")
-
-
-
-#valor localidad de inicio de valor 0
-# si el usuario pone el nombre de la localidad cuando llama a la funcion se crea un archivo csv de la localidad llamada
-#si el usuario no pone el nombre de la localidad la funcion hace un archivo csv general de todo 
-
-
-#trabajar con datos del gdf
